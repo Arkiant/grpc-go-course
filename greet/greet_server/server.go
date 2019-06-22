6f6fcd2db36867d42529b98c6de5b138fa0d6e0c
Samuel Porras <samuelporras@gmail.com>
2019-06-22 16:10:35 +0200

    Created grpc server

---
+++ greet/greet_server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/arkiant/grpc-go-course/greet/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}

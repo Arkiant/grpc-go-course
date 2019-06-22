0d6ec418a60408b72fd70609a09e6f11fa26c824
Samuel Porras <samuelporras@gmail.com>
2019-06-22 21:16:43 +0200

    Server Streaming API Server Implementation

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/arkiant/grpc-go-course/greet/greetpb"
 
@@ -24,6 +26,23 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 	}
 
 	return res, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+
+	fmt.Printf("GreetManyTimes function was invoked with %v\n", req)
+
+	firstName := req.GetGreeting().GetFirstName()
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstName + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
 }
 
 func main() {

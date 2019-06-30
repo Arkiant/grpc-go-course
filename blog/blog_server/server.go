d43722d9cbe4bf445e69991fb1469858fa3db38c
Samuel Porras <samuelporras@gmail.com>
2019-06-30 21:48:16 +0200

    Extra: Update code and separate functionality

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -14,51 +14,28 @@ import (
 	"github.com/arkiant/grpc-go-course/blog/blogpb"
 
 	"github.com/arkiant/grpc-go-course/blog/database"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc"
 )
 
 type server struct{}
 
-type blogItem struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	AuthorID string             `bson:"author_id"`
-	Content  string             `bson:"content"`
-	Title    string             `bson:"title"`
-}
-
 func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*blogpb.CreateBlogResponse, error) {
 
 	blog := req.GetBlog()
 
-	data := blogItem{
-		AuthorID: blog.GetAuthorId(),
-		Content:  blog.GetContent(),
-		Title:    blog.GetContent(),
-	}
+	data := database.CreateBlog(blog.GetAuthorId(), blog.GetTitle(), blog.GetContent())
 
-	collection := database.MongoCollection()
-
-	res, err := collection.InsertOne(context.Background(), data)
+	oid, err := database.InsertOne(data)
 	if err != nil {
 		return nil, status.Errorf(
 			codes.Internal,
-			fmt.Sprintf("Internal error: %v", err),
-		)
-	}
-
-	oid, ok := res.InsertedID.(primitive.ObjectID)
-	if !ok {
-		return nil, status.Errorf(
-			codes.Internal,
-			fmt.Sprintf("Cannot convert to OID"),
+			err.Error(),
 		)
 	}
 
 	return &blogpb.CreateBlogResponse{
 		Blog: &blogpb.Blog{
-			Id:       oid.Hex(),
+			Id:       oid,
 			AuthorId: blog.GetAuthorId(),
 			Title:    blog.GetTitle(),
 			Content:  blog.GetContent(),
@@ -69,36 +46,22 @@ func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*
 
 func (*server) ReadBlog(ctx context.Context, req *blogpb.ReadBlogRequest) (*blogpb.ReadBlogResponse, error) {
 	blogID := req.GetBlogId()
-	oid, err := primitive.ObjectIDFromHex(blogID)
+	data, err := database.FindOneByID(blogID)
+
 	if err != nil {
 		return nil, status.Errorf(
-			codes.InvalidArgument,
-			fmt.Sprintf("Cannot parse ID"),
-		)
-	}
-
-	// create an empty struct
-	data := &blogItem{}
-
-	// create a filter, this filter is the where clause in a relational database
-	filter := bson.D{
-		bson.E{
-			Key:   "_id",
-			Value: oid,
-		},
-	}
-
-	collection := database.MongoCollection()
-	res := collection.FindOne(context.Background(), filter)
-	if err := res.Decode(data); err != nil {
-		return nil, status.Errorf(
-			codes.NotFound,
-			fmt.Sprintf("Cannot find blog with specified ID: %v\n", err),
+			codes.Internal,
+			err.Error(),
 		)
 	}
 
 	return &blogpb.ReadBlogResponse{
-		Blog: dataToBlogPb(data),
+		Blog: &blogpb.Blog{
+			Id:       data.ID.Hex(),
+			AuthorId: data.AuthorID,
+			Title:    data.Title,
+			Content:  data.Content,
+		},
 	}, nil
 
 }
@@ -106,60 +69,24 @@ func (*server) ReadBlog(ctx context.Context, req *blogpb.ReadBlogRequest) (*blog
 func (*server) UpdateBlog(ctx context.Context, req *blogpb.UpdateBlogRequest) (*blogpb.UpdateBlogResponse, error) {
 	blog := req.GetBlog()
 
-	oid, err := primitive.ObjectIDFromHex(blog.GetId())
-	if err != nil {
-		return nil, status.Errorf(
-			codes.InvalidArgument,
-			fmt.Sprintf("Cannot parse ID"),
-		)
-	}
+	data := database.CreateBlog(blog.GetAuthorId(), blog.GetContent(), blog.GetTitle())
 
-	// create an empty struct
-	data := &blogItem{}
-
-	// create a filter, this filter is the where clause in a relational database
-	filter := bson.D{
-		bson.E{
-			Key:   "_id",
-			Value: oid,
-		},
-	}
-
-	collection := database.MongoCollection()
-	res := collection.FindOne(context.Background(), filter)
-	if err := res.Decode(data); err != nil {
-		return nil, status.Errorf(
-			codes.NotFound,
-			fmt.Sprintf("Cannot find blog with specified ID: %v\n", err),
-		)
-	}
-
-	// We update our internal struct
-	data.AuthorID = blog.GetAuthorId()
-	data.Content = blog.GetContent()
-	data.Title = blog.GetTitle()
-
-	_, updateError := collection.ReplaceOne(context.Background(), filter, data)
-	if updateError != nil {
+	if updateError := database.ReplaceOneByID(data, blog.GetId()); updateError != nil {
 		return nil, status.Errorf(
 			codes.Internal,
-			fmt.Sprintf("Cannot update object in MongoDB: %v\n", updateError),
+			updateError.Error(),
 		)
 	}
 
 	return &blogpb.UpdateBlogResponse{
-		Blog: dataToBlogPb(data),
+		Blog: &blogpb.Blog{
+			Id:       data.ID.Hex(),
+			AuthorId: data.AuthorID,
+			Title:    data.Title,
+			Content:  data.Content,
+		},
 	}, nil
 
-}
-
-func dataToBlogPb(data *blogItem) *blogpb.Blog {
-	return &blogpb.Blog{
-		Id:       data.ID.Hex(),
-		AuthorId: data.AuthorID,
-		Title:    data.Title,
-		Content:  data.Content,
-	}
 }
 
 func main() {
--- blog/database/mongo.go
+++ blog/database/mongo.go
@@ -2,9 +2,12 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -13,6 +16,13 @@ var (
 	client     *mongo.Client
 	collection *mongo.Collection
 )
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
 
 func openConnection() *mongo.Client {
 	if client == nil {
@@ -24,6 +34,14 @@ func openConnection() *mongo.Client {
 		return client
 	}
 	return client
+}
+
+func getOid(id string) (primitive.ObjectID, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return primitive.ObjectID{}, fmt.Errorf(fmt.Sprintf("Cannot parse ID"))
+	}
+	return oid, nil
 }
 
 /*
@@ -46,4 +64,84 @@ func CloseConnection() {
 	if client != nil {
 		client.Disconnect(context.TODO())
 	}
+}
+
+// CreateBlog is a function for create a blog new blog instance
+func CreateBlog(authorID string, title string, content string) *blogItem {
+	return &blogItem{
+		AuthorID: authorID,
+		Title:    title,
+		Content:  content,
+	}
+}
+
+// InsertOne insert a row in database and return a oid and error
+func InsertOne(data *blogItem) (string, error) {
+	res, err := collection.InsertOne(context.Background(), data)
+	if err != nil {
+		return "", fmt.Errorf(fmt.Sprintf("Internal error: %v", err))
+	}
+
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return "", fmt.Errorf(fmt.Sprintf("Cannot convert to OID"))
+	}
+
+	return oid.Hex(), nil
+}
+
+// FindOneByID find a collection by id
+func FindOneByID(id string) (*blogItem, error) {
+
+	oid, err := getOid(id)
+	if err != nil {
+		return nil, err
+	}
+
+	// create an empty struct
+	data := &blogItem{}
+
+	// create a filter, this filter is the where clause in a relational database
+	filter := bson.D{
+		bson.E{
+			Key:   "_id",
+			Value: oid,
+		},
+	}
+
+	res := collection.FindOne(context.Background(), filter)
+	if err := res.Decode(data); err != nil {
+		return nil, fmt.Errorf(fmt.Sprintf("Cannot find blog with specified ID: %v\n", err))
+	}
+
+	return data, nil
+}
+
+// ReplaceOneByID replace data
+func ReplaceOneByID(dataUpdate *blogItem, id string) error {
+
+	data, err := FindOneByID(id)
+
+	oid, err := getOid(id)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.D{
+		bson.E{
+			Key:   "_id",
+			Value: oid,
+		},
+	}
+
+	data.AuthorID = dataUpdate.AuthorID
+	data.Title = dataUpdate.Title
+	data.Content = dataUpdate.Content
+
+	_, updateError := collection.ReplaceOne(context.Background(), filter, data)
+	if updateError != nil {
+		return fmt.Errorf(fmt.Sprintf("Cannot update object in MongoDB: %v\n", updateError))
+	}
+
+	return nil
 }

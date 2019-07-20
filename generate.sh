d0f9f469a8f5fbbaf48ae0a5bfc53a79a9d9ecac
Arkiant <samuelporras@gmail.com>
2019-07-20 13:36:09 +0200

    Added auth service .proto

--- generate.sh
+++ generate.sh
@@ -3,3 +3,4 @@
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
 protoc calculator/pb/calculator.proto --go_out=plugins=grpc:.
 protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
+protoc auth/authpb/auth.proto --go_out=plugins=grpc:.
